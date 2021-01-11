dc614a2a340c8391ee0cbc91fd8838303817700f
Bora Kim <stellakkkk@hotmail.com>
2021-01-11 18:55:13 +0000

    Understanding the base features and syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+// import React, { useState } from 'react';
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+// import person from './Person/Person';
+
 
 // function App() {
 //   return (
@@ -10,16 +13,108 @@ import Person from './Person/Person';
 //   );
 // }
 //
+// const App = props =>  {
+//   const [ personsState, setPersonsState ] = useState({
+//         persons: [
+//           { name: 'Max', age: 28},
+//           { name: 'Manu', age: 23},
+//           { name: 'Ste', age: 20}
+//         ],
+//         otherState: 'some other value'
+//       });
+
+
+// const [otherState, setOtherState] = useState('djkfljdlkjkls');
+
+// console.log(personsState, otherState);
+
+
+//   const switchNameHandler = () => {
+//     setPersonsState(
+//       {persons: [
+//       { name: 'Maxidifjidji', age: 28},
+//       { name: 'Manu', age: 23},
+//       { name: 'Ste', age: 230}
+//     ],
+//     otherState: personsState.otherState
+//   })
+// }
+
+//   return (
+//       <div className="App">
+//         <h1>Hi, I am react app </h1>
+//         <p>This is working 1</p>
+//         <button onClick={switchNameHandler}>Switch Name</button>
+//         <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing </Person>
+//         <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+//       </div>
+//     );
+// };
+
+//   export default App;
+
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name : "Manu", age : 29 },
+      { name : "Step", age : 26 }
+    ],
+    otherState: "some other value"
+  }
+  // event handler
+  switchNameHandler = (newName) => {
+    // I want to add state
+    // console.log("Was clicked!")
+    // this this would refer sate with ES6 function
+    // this.state.persons[0].name = "Maximailian"
+    this.setState({ persons : [
+      { name: newName, age: 28 },
+      { name : "Manu", age : 29 },
+      { name : "Step", age : 30 }
+    ]})
+  }
+
+  nameChangeHandler = (event) =>{
+    console.log(event.target.value);
+    this.setState({ 
+      persons : [
+        { name:  "Max", age: 28 },
+        { name : event.target.value , age : 29 },
+        { name : "Step", age : 26 }
+    ]})
+  }
+
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
-      <div className="App">
-        <h1>Hi, I am react app </h1>
-        <p>This is working 1</p>
-        <Person />
+      <div className = "App">
+         <h1> Hi I am a react app</h1> 
+         <h2> Reactt</h2>
+        <button 
+          style={style}
+          onClick = {() => this.switchNameHandler('dkdkkdd')} >Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age ={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age ={this.state.persons[1].age} click = {this.switchNameHandler.bind(this, 'Lina')}
+          changed={this.nameChangeHandler}>My Hobby : racing </Person> 
+        <Person 
+          name={this.state.persons[2].name} 
+          age ={this.state.persons[2].age}/>
       </div>
     );
-    // return React.createElement('div',  {className:'App'}, React.createElement('h1', null, 'Hi I am reactttttt'))
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,26 @@
+// import { render } from '@testing-library/react';
 import React from 'react';
+import './Person.css';
 
-const person = () => {
-    return <p>I'm a person</p>
+// const person = (props) => {
+//     return (
+//         <div>
+//             <p>I'm a {props.name} and I am {props.age} years old</p>
+//             <p>{props.children}</p>
+//         </div>
+//     )
+
+// };
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+
 };
 
 export default person;
